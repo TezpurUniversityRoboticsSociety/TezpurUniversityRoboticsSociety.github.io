cce4f4ef29afb1307974e630303e9b76eab71e78
AdirthaBorgohain <adirthaborgohain@gmail.com>
2018-02-15 23:40:39 +0530

    redesigned style to material design

--- js/animation-main.js
+++ js/animation-main.js
@@ -113,7 +113,7 @@ function loop() {
 	
 	RADIUS_SCALE = Math.min( RADIUS_SCALE, RADIUS_SCALE_MAX );
 	
-	context.fillStyle = 'rgba(0,0,0,0.4)';
+	context.fillStyle = 'rgba(0,0,0,0.6)';
 		 context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 	
 	for (i = 0, len = particles.length; i < len; i++) {
--- js/text_anime.js
+++
@@ -1,5 +0,0 @@
-$("#frst").fadeIn(1500,function(){
-	$("#scnd").fadeIn(1000,function(){
-		$("#thrd").fadeIn(2000);
-	});
-});
