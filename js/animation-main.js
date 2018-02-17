0494dce1541a4bac323cc68ce10af29d67b2450b
nityanandagohain <nityanandagohain@gmail.com>
2018-02-17 09:00:53 +0530

    landing page and navbar updated

--- js/animation-main.js
+++ js/animation-main.js
@@ -3,13 +3,20 @@ $(document).ready(function() {
   	if($(document).scrollTop() > 400) {
 		$('.navbar').addClass('shrink');
 		$('.layer2text').addClass('fadeInUp');
+		$('.nav-link').removeClass('navlinkcol1')
+		$('.nav-link').addClass('navlinkcol2');
     }
     else {
 		$('.navbar').removeClass('shrink');
 		$('.layer2text').removeClass('fadeInUp');
+
+		$('.nav-link').addClass('navlinkcol1');
+		$('.nav-link').removeClass('navlinkcol2');
 	}
   });
 });
+
+
 
 $(document).ready(function() {
 	$('.slide-section').click(function(e) {
